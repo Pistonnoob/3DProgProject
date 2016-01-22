36d363bc2a34d74bdd20c021ea8e4b67038e56c7
Pistonnoob <kimeld@hotmail.com>
2016-01-22 15:36:19 +0100

    Added main() and linked  to SDL

--- main.cpp
+++ main.cpp
@@ -1,2 +1,12 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <iostream>
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << " Failed to initialize SDL : " << SDL_GetError() << std::endl;
+		return -1;
+	}
+
+}
